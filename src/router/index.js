e61d6766bb941dafe89c4664023123a5f0ebf878
daqs8023 <364798172@qq.com>
2020-03-14 16:34:14 +0800

    笔记“

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ import Router from 'vue-router'
 import Login from '@/components/login/login'
 import Home from '@/components/home/home'
 import Users from '@/components/users/users'
-
+import Right from '@/components/rights/right'
+import Role from '@/components/rights/role'
+import {Message} from'element-ui'
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   routes: [
     {
       name:'login',
@@ -17,11 +19,52 @@ export default new Router({
       name:'home',
       path: '/',
       component:Home,
-      children:[{
-        name:'users',
-        path: 'users',
-        component:Users,
-      }]
-    }
+      children:[
+        {
+          name:'users',
+          path: 'users',
+          component:Users,
+        },
+        {
+          name:'right',
+          path: '/right',
+          component:Right
+        },
+        {
+          name:'role',
+          path: '/role',
+          component:Role
+        }
+      ]
+    }    
   ]
 })
+
+// 在路由配置生效之前 统一判断token
+// 路由守卫 在路由哦诶之生效之前
+// 路由/导航 守卫
+// to -> 要去的路由配置
+// from -> 当前的路由配置
+router.beforeEach((to,from,next) =>{
+  //console.log(to,from)
+  //如果要去的是登录 -> next()
+  if(to.path === '/login'){
+    next()
+  }else{
+    //如果要去的不是登录
+    //判断token
+    const token = localStorage.getItem('token')
+    if(!token){
+      //如果token没有 -> login
+      Message.warning('请先登录')
+
+      router.push({name:'login'})
+      return
+    }
+    next()
+  }
+})
+export default router
+// 在路由配置生效之前 统一判断token
+
+// 路由/导航 守卫
